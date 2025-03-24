b275e4ecdd163e0289f730c6c42ffc066a8df88f
2519 <2519167027@qq.com>
2025-03-24 23:29:39 +0800

    add:使用lua脚本，实现分布式锁拿锁比锁判锁原子性

--- src/main/java/com/hmdp/utils/SimpleRedisLock.java
+++ src/main/java/com/hmdp/utils/SimpleRedisLock.java
@@ -3,8 +3,11 @@ package com.hmdp.utils;
 import cn.hutool.core.lang.UUID;
 import cn.hutool.core.util.BooleanUtil;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.ClassPathResource;
 import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.data.redis.core.script.DefaultRedisScript;
 
+import java.util.Collections;
 import java.util.concurrent.TimeUnit;
 
 public class SimpleRedisLock implements ILock{
@@ -14,6 +17,13 @@ public class SimpleRedisLock implements ILock{
 
     private StringRedisTemplate stringRedisTemplate;
     private String name;
+
+    private static final DefaultRedisScript<Long> UNLOCK_SCRIPT;
+    static {
+        UNLOCK_SCRIPT = new DefaultRedisScript<>();
+        UNLOCK_SCRIPT.setLocation(new ClassPathResource("unlock.lua"));
+        UNLOCK_SCRIPT.setResultType(Long.class);
+    }
 
     public SimpleRedisLock(String name, StringRedisTemplate stringRedisTemplate) {
         this.name = name;
@@ -33,10 +43,21 @@ public class SimpleRedisLock implements ILock{
 
     @Override
     public void unlock() {
-        String threadId=ID_PREFIX+Thread.currentThread().getId();
-        String id=stringRedisTemplate.opsForValue().get(KEY_PREFIX+name);
-        if(threadId.equals(id)) {
-            stringRedisTemplate.delete(KEY_PREFIX + name);
-        }
+        //调用lua脚本,实现拿锁比锁删锁三个操作原子性
+        stringRedisTemplate.execute(
+                UNLOCK_SCRIPT,
+                Collections.singletonList(KEY_PREFIX+name),
+                ID_PREFIX+Thread.currentThread().getId()
+        );
     }
+
+    //    @Override
+//    public void unlock() {
+//        String threadId=ID_PREFIX+Thread.currentThread().getId();
+//        String id=stringRedisTemplate.opsForValue().get(KEY_PREFIX+name);
+//        if(threadId.equals(id)) {
+//            //判断锁和删锁是两个操作，删锁不具备原子性，极端情况下也会导致误删
+//            stringRedisTemplate.delete(KEY_PREFIX + name);
+//        }
+//    }
 }
