ae6bd071909cb13cb9aa99729e6b133416d04864
2519 <2519167027@qq.com>
2025-03-24 14:44:10 +0800

    add:使用乐观锁解决秒杀券库存超卖

--- src/main/java/com/hmdp/service/impl/VoucherOrderServiceImpl.java
+++ src/main/java/com/hmdp/service/impl/VoucherOrderServiceImpl.java
@@ -53,9 +53,15 @@ public class VoucherOrderServiceImpl extends ServiceImpl<VoucherOrderMapper, Vou
         }
 
         //减少库存
+        //下面这种乐观锁实现方式效率比较差，虽然安全，但是会导致卖出的券很少，因为卖出的概率很低
+//        boolean success = seckillVoucherService.update()
+//                .setSql("stock = stock -1")
+//                .eq("voucher_id", voucherId).eq("stock",voucher.getStock()).update();
+
+        //大于0就可以卖
         boolean success = seckillVoucherService.update()
                 .setSql("stock = stock -1")
-                .eq("voucher_id", voucherId).update();
+                .eq("voucher_id", voucherId).gt("stock",0).update();
 
         if(!success){
             return Result.fail("库存不足！");
