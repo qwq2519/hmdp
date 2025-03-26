38b4f64d0965f4bd027735f7344ca430f8be458d
2519 <2519167027@qq.com>
2025-03-26 09:14:02 +0800

    fixed:批量生成登录用户

--- src/test/java/com/hmdp/UserLoginBatch.java
+++ src/test/java/com/hmdp/UserLoginBatch.java
@@ -32,43 +32,38 @@ public class UserLoginBatch {
     private StringRedisTemplate stringRedisTemplate;
 
     @Test
-    public void getAllTest() {
+    public void generateBatchUserTokens() {
         List<User> users = userService.list();
-        System.out.println(users.size());
-        users.forEach(
-                user -> {
-                    //随机生成token,作为登录令牌
-                    String token = UUID.randomUUID().toString(true);
-                    // 将User对象转化为HashMap存储
-                    UserDTO userDTO = BeanUtil.copyProperties(user, UserDTO.class);
 
-                    File file = new File("src/main/resources/token.txt");
-                    FileOutputStream output = null;
-                    try {
-                        output = new FileOutputStream(file, true);
-                        byte[] bytes = token.getBytes();
-                        output.write(bytes);
-                        output.write("\r\n".getBytes());
-                    } catch (Exception e) {
-                        throw new RuntimeException(e);
-                    } finally {
-                        try {
-                            output.close();
-                        } catch (IOException e) {
-                            throw new RuntimeException(e);
-                        }
-                    }
-                    Map<String, Object> userMap = BeanUtil.beanToMap(userDTO, new HashMap<>(),
-                            CopyOptions.create()
-                                    .setIgnoreNullValue(true)
-                                    .setFieldValueEditor((fieldName, fieldValue) -> fieldValue.toString()));
-                    //  7.3,存储
-                    String tokenKey = LOGIN_USER_KEY + token;
-                    stringRedisTemplate.opsForHash().putAll(tokenKey, userMap);
-                    //  7.4,设置token有效期
-                    stringRedisTemplate.expire(tokenKey, LOGIN_USER_TTL, TimeUnit.MINUTES);
-                }
-        );
+        File file = new File("src/main/resources/token.txt");
+
+        try (FileOutputStream output = new FileOutputStream(file, false)) { // false to overwrite
+            for (int i = 0; i < users.size(); i++) { // Process all users
+                User user = users.get(i);
+
+                // Generate token
+                String token = UUID.randomUUID().toString(true);
+
+                // Convert to UserDTO
+                UserDTO userDTO = BeanUtil.copyProperties(user, UserDTO.class);
+
+                // Write token to file
+                output.write(token.getBytes());
+                output.write("\r\n".getBytes());
+
+                // Store in Redis
+                Map<String, Object> userMap = BeanUtil.beanToMap(userDTO, new HashMap<>(),
+                        CopyOptions.create()
+                                .setIgnoreNullValue(true)
+                                .setFieldValueEditor((fieldName, fieldValue) -> fieldValue.toString()));
+
+                String tokenKey = LOGIN_USER_KEY + token;
+                stringRedisTemplate.opsForHash().putAll(tokenKey, userMap);
+                stringRedisTemplate.expire(tokenKey, LOGIN_USER_TTL, TimeUnit.MINUTES);
+            }
+        } catch (IOException e) {
+            throw new RuntimeException("Failed to write tokens to file", e);
+        }
     }
 
 
