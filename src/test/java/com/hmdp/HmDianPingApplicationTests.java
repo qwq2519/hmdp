a225a91b0ce716c0841b38b1a67d8e3fc3cfa5a9
2519 <2519167027@qq.com>
2025-03-28 22:03:20 +0800

    add:单测导入店铺经纬度到redis

--- src/test/java/com/hmdp/HmDianPingApplicationTests.java
+++ src/test/java/com/hmdp/HmDianPingApplicationTests.java
@@ -14,8 +14,15 @@ import org.redisson.api.RedissonClient;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceTransactionManagerAutoConfiguration;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.geo.Point;
+import org.springframework.data.redis.connection.RedisGeoCommands;
+import org.springframework.data.redis.core.StringRedisTemplate;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
 import java.util.concurrent.*;
+import java.util.stream.Collectors;
 
 @SpringBootTest
 class HmDianPingApplicationTests {
@@ -31,6 +38,9 @@ class HmDianPingApplicationTests {
 
     @Autowired
     private RedissonClient redisClient;
+
+    @Autowired
+    private StringRedisTemplate stringRedisTemplate;
 
     private ExecutorService es = Executors.newFixedThreadPool(500);
 
@@ -70,6 +80,31 @@ class HmDianPingApplicationTests {
                 lock.unlock();
             }
         }
+    }
+
+    @Test
+    void loadShopData(){
+        List<Shop> list = shopService.list();
+        // 店铺分组，按照typeId分组
+        Map<Long, List<Shop>> map = list.stream().collect(Collectors.groupingBy(Shop::getTypeId));
+
+        //分批写入redis
+        for (Map.Entry<Long, List<Shop>> entry : map.entrySet()) {
+            Long typeId = entry.getKey();
+            String redisKey=RedisConstants.SHOP_GEO_KEY+typeId;
+
+            List<Shop> value = entry.getValue();
+            List<RedisGeoCommands.GeoLocation<String>> locations=new ArrayList<>(value.size());
+
+            for (Shop shop : value) {
+                locations.add(new RedisGeoCommands.GeoLocation<>(
+                        shop.getId().toString(),new Point(shop.getX(),shop.getY())
+                ));
+            }
+            stringRedisTemplate.opsForGeo().add(redisKey,locations);
+
+        }
+
     }
 
 }
